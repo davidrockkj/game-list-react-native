503d2cbb0ebc0711c847eaf013189823c21bc7a7
davidrockkj <davidrockkj@gmail.com>
2024-01-23 16:06:23 -0300

    Estruturando 'Header > index.tsx'

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, TextInput } from 'react-native';
+import { useState } from 'react';
+import styles from './styles';
+
+function Header () {
+  return (
+
+  )
+}
+
+export default Header;
