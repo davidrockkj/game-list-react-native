b0e912c99019699756c5a5a263cc67bb4a3112bf
davidrockkj <davidrockkj@gmail.com>
2024-01-23 17:05:34 -0300

    Importando a lista de games + criando 'GameList'

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useState } from 'react';
+import games, { IGames } from './src/data/games';
 import Header from './src/components/Header';
 import Footer from './src/components/Footer';
 import styles from './AppStyles';
 
 export default function App() {
+  const [filteredGames, setFilteredGames] = useState<IGames[]>(games);
+
   return (
     <View style={styles.container}>
       <Header />
