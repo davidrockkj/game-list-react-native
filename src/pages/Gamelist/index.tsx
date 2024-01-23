d119d64f3152cee1b509dc24668cce1e9c37fedc
davidrockkj <davidrockkj@gmail.com>
2024-01-23 17:08:23 -0300

    Começando a estruturar 'GameList'

--- src/pages/Gamelist/index.tsx
+++ src/pages/Gamelist/index.tsx
@@ -1,7 +1,12 @@
 import { View, Text, FlatList } from 'react-native';
+import GameCard from '../../components/GameCard';
+import { IGames } from '../../data/games';
+import styles from './styles';
 
 
 function GameList() {
+  
+
   return (
 
   )
