f435adb08aabe481d812172e0120717e7b3626d0
davidrockkj <davidrockkj@gmail.com>
2024-01-23 16:24:48 -0300

    Adicionando dados dos jogos

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import Header from './src/components/Header';
+import Footer from './src/components/Footer';
 import styles from './AppStyles';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Footer />
       <StatusBar style="light" />
     </View>
   );
