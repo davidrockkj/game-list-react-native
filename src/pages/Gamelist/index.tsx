78df174b3e3e911e279d217bcb0f948f2449abf7
davidrockkj <davidrockkj@gmail.com>
2024-01-23 17:18:37 -0300

    Tipagem: GameList, ItemList // Criando função 'renderGame' que irá renderizar cada card com um item da lista através do FlatList

--- src/pages/Gamelist/index.tsx
+++ src/pages/Gamelist/index.tsx
@@ -3,12 +3,35 @@ import GameCard from '../../components/GameCard';
 import { IGames } from '../../data/games';
 import styles from './styles';
 
+// Tipando os games
+interface GameListProps {
+  games: IGames[]
+};
 
-function GameList() {
+function GameList({games}: GameListProps) {
+
+  // Criando tipagem para a função renderGame
+  interface ItemList {
+    item: IGames;
+  }
+
+  // Função responsável por renderizar cada um dos jogos
+  const renderGame = ({item}: ItemList) => (
+    <GameCard game={item}/>
+  )
   
 
   return (
-
+    <View style={styles.content}>
+      <Text style={styles.title}>Jogos adicionados recentemente</Text>
+      <FlatList
+        style={styles.flatList}
+        showsVerticalScrollIndicator={false}
+        data={games}
+        renderItem={renderGame}
+        keyExtractor={item => String(item.id)}
+        />
+    </View>
   )
 }
 
