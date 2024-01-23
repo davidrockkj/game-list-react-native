74f1229f8fb963df02418671a057cbf1ecc8cf98
davidrockkj <davidrockkj@gmail.com>
2024-01-23 16:19:04 -0300

    Estruturando 'Footer > index.tsx'

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import Header from './src/components/Header';
 import styles from './AppStyles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Deixa eu dar uma testadinha</Text>
+      <Header />
       <StatusBar style="light" />
     </View>
   );
