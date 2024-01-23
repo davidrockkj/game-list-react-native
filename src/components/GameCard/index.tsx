4d6dbdc416d25f5487ee54c37e63c376c197a2a3
davidrockkj <davidrockkj@gmail.com>
2024-01-23 17:06:59 -0300

    Criando 'GameCard'

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+function GameCard() {
+  return (
+    
+  )
+};
+
+export default GameCard;
---
+++ src/pages/Gamelist/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, FlatList } from 'react-native';
+
+
+function GameList() {
+  return (
+
+  )
+}
+
+export default GameList;
