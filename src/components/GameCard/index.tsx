44e7242d8719e3fd07dcd2c4da94f65c13b12c03
davidrockkj <davidrockkj@gmail.com>
2024-01-23 18:57:56 -0300

    Finalizando 'GameCard'

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,8 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { IGames } from "../../data/games";
+import styles from "../Header/styles";
 
-function GameCard() {
+interface GameCardProps {
+  game: IGames
+}
+
+function GameCard({game}: GameCardProps) {
+
+  // Função para retornar estrelas na avaliação
+  const generateStarRating = (ratingScore: number) => {
+    const numStars = Math.floor(ratingScore);
+    return "⭐".repeat(numStars);
+  };
+
   return (
-    
+    <View style={styles.card}>
+      <Image source={{ uri: game.image }} />
+      <Text style={styles.title}>{game.name}</Text>
+      <Text style={styles.data}>{`Pataforma: ${game.platform}`}</Text>
+      <Text style={styles.data}>{`Gênero: ${game.genre}`}</Text>
+      <Text style={styles.data}>{`Data de Lançamento: ${game.releaseDate}`}</Text>
+      <Text style={styles.data}>{`Público: ${game.rating}`}</Text>
+      <Text style={styles.data}>{`Empresa: ${game.developer}`}</Text>
+      <Text style={styles.data}>{generateStarRating(game.ratingScore)}</Text>
+
+    </View>
   )
 };
 
--- src/pages/Gamelist/styles.ts
+++ src/pages/Gamelist/styles.ts
@@ -22,3 +22,5 @@ const styles = StyleSheet.create({
     borderBottomColor: '#000',
   },
 })
+
+export default styles;
