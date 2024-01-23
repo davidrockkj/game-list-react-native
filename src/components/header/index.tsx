933bce226cb3f8c13f80032f0283b9f45422cfae
davidrockkj <davidrockkj@gmail.com>
2024-01-23 16:09:17 -0300

    Criando o useState 'filter' e 'handleFilter'

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,14 @@ import { useState } from 'react';
 import styles from './styles';
 
 function Header () {
+  // Filtrar o jogo
+  const [filter, setFilter] = useState<string>('');
+
+  // Fazer o uso da função para filtrar
+  const handleFilter = (text: string) => {
+    setFilter(text);
+  };
+
   return (
 
   )
