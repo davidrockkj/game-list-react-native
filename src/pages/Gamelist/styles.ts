354e00e4c8be8fb4f99f4aeb1cc90e404e8b7436
davidrockkj <davidrockkj@gmail.com>
2024-01-23 17:22:30 -0300

    Estilizando 'GameList'

---
+++ src/pages/Gamelist/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    width: '100%',
+    paddingBottom: 24,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  flatList: {
+    width: '80%',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingBottom: 4,
+    textTransform: 'uppercase',
+    margin: 24,
+    color: '#000',
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
+  },
+})
